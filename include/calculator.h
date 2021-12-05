6ba41a89d7573d7aaa69138144e5272f5b4ccc01
-=Oktan=- <r.zhigalenko@mail.ru>
2021-12-05 23:36:29 +0500

    completed

--- include/calculator.h
+++ include/calculator.h
@@ -1,17 +1,30 @@
 #pragma once
 #include <QMainWindow>
 #include <QLineEdit>
+#include <qstring.h>
+#include "ui_calc.h"
+
+enum class OPERATOR
+{
+    ADD,
+    SUB,
+    MULT,
+    DIV
+};
 
 class Calcwindow : public QMainWindow
 {
     Q_OBJECT
-public:
     QLineEdit *_answer = nullptr;
     QLineEdit *_number_1 = nullptr;
     QLineEdit *_number_2 = nullptr;
-    
-    Calcwindow(QWidget *parent) : QMainWindow(parent) {}
+    Ui::CalcWindow *calc = nullptr;
     void setupLineEdit(QLineEdit *answer, QLineEdit *number_1, QLineEdit *number_2);
+    QString calculate(const OPERATOR& op);
+public:
+    
+    explicit Calcwindow(QWidget *parent);
+    ~Calcwindow();
 
 public slots:
     void add();
--- src/calculator.cpp
+++ src/calculator.cpp
@@ -1,5 +1,6 @@
 #include "calculator.h"
-#include <qstring.h>
+
+
 
 void Calcwindow::setupLineEdit(QLineEdit *answer, QLineEdit *number_1, QLineEdit *number_2)
 {
@@ -8,32 +9,78 @@ void Calcwindow::setupLineEdit(QLineEdit *answer, QLineEdit *number_1, QLineEdit
     _number_2 = number_2;
 }
 
+Calcwindow::Calcwindow(QWidget *parent) : QMainWindow(parent)
+{
+    calc = new Ui::CalcWindow;
+    calc->setupUi(this);
+    setupLineEdit(calc->answer, calc->number_1, calc->number_2);
+    show();
+}
+
+Calcwindow::~Calcwindow()
+{
+    delete calc;
+}
+
+QString Calcwindow::calculate(const OPERATOR& op)
+{
+    bool ok_num1;
+    qlonglong num1 = _number_1->text().toLongLong(&ok_num1);
+    bool ok_num2;
+    qlonglong num2 = _number_2->text().toLongLong(&ok_num2);
+    QString answer;
+
+    if(ok_num1 && ok_num2)
+    {
+        switch (op)
+        {
+        case OPERATOR::ADD :
+            answer =  QString::number(num1 + num2);
+            break;
+        case OPERATOR::SUB :
+            answer =  QString::number(num1 - num2);
+            break;
+        case OPERATOR::MULT :
+            answer =  QString::number(num1 * num2);
+            break;
+        case OPERATOR::DIV :
+            if(num2 == 0)
+            {
+                answer = "Error";
+                break;
+            }
+            answer =  QString::number(num1 / num2);
+            break;
+        
+        default:
+            answer = "0";
+            break;
+        }
+        
+    }
+    else
+    {
+        answer = "Error";
+    }  
+    return answer;
+}
+
 void Calcwindow::add()
 {
-   
-    qlonglong num1 = _number_1->text().toInt();
-    qlonglong num2 = _number_2->text().toInt();   
-    _answer->setText(QString::number(num1 + num2));
-    
+     _answer->setText("= " + calculate(OPERATOR::ADD));
 }
 
 void Calcwindow::sub()
 {
-    qlonglong num1 = _number_1->text().toInt();
-    qlonglong num2 = _number_2->text().toInt();   
-    _answer->setText(QString::number(num1 - num2));
+    _answer->setText("= " + calculate(OPERATOR::SUB));
 }
 
 void Calcwindow::mult()
 {
-    qlonglong num1 = _number_1->text().toInt();
-    qlonglong num2 = _number_2->text().toInt();   
-    _answer->setText(QString::number(num1 * num2));
+    _answer->setText("= " + calculate(OPERATOR::MULT));
 }
 
 void Calcwindow::div()
 {
-    qlonglong num1 = _number_1->text().toInt();
-    qlonglong num2 = _number_2->text().toInt();   
-    _answer->setText(QString::number(num1 / num2));
+    _answer->setText("= " + calculate(OPERATOR::DIV));
 }
